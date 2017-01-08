2160d35b5bd0ffc38dcce63d0291be171fffef92
tking2005 <tking2005@gmail.com>
2017-01-08 18:19:25 -0500

    first commit

--- app/src/main/java/nyc/c4q/shannonalexander_navarro/memestudio/CreateMemeActivity.java
+++ app/src/main/java/nyc/c4q/shannonalexander_navarro/memestudio/CreateMemeActivity.java
@@ -1,13 +1,32 @@
 package nyc.c4q.shannonalexander_navarro.memestudio;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateMemeActivity extends AppCompatActivity {
+
+    FloatingActionButton home_fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_meme);
+
+        home_fab = (FloatingActionButton)findViewById(R.id.home);
+        home_fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent homeIntent = new Intent(CreateMemeActivity.this, MainActivity.class);
+                startActivity(homeIntent);
+            }
+
+        });
+
+
     }
+
+
 }
