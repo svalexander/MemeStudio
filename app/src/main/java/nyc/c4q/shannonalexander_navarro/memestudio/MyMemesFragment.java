5755c27c256a820473f759c10648caa7987d2cc9
Shannon Alexander-Navarro <shannonalexander@hotmail.com>
2017-01-08 18:40:02 -0500

    fab closes frag

--- app/src/main/java/nyc/c4q/shannonalexander_navarro/memestudio/MyMemesFragment.java
+++ app/src/main/java/nyc/c4q/shannonalexander_navarro/memestudio/MyMemesFragment.java
@@ -1,7 +1,9 @@
 package nyc.c4q.shannonalexander_navarro.memestudio;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +17,7 @@ import android.widget.LinearLayout;
 public class MyMemesFragment extends Fragment {
     LinearLayout memeLayout;
 
+    FloatingActionButton closeRVFrag;
 
     @Nullable
     @Override
@@ -22,7 +25,15 @@ public class MyMemesFragment extends Fragment {
 
         View root = inflater.inflate(R.layout.my_memes_fragment, container, false);
         memeLayout = (LinearLayout) root.findViewById(R.id.saved_memes_frag);
+        closeRVFrag = (FloatingActionButton) root.findViewById(R.id.close_rv);
 
+        closeRVFrag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent closeIntent = new Intent(getContext(), MainActivity.class);
+                startActivity(closeIntent);
+            }
+        });
         return root;
     }
 }
