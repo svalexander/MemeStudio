3f4ac48911387111bea889c777594e98e05e414b
Shannon Alexander-Navarro <shannonalexander@hotmail.com>
2017-01-09 19:54:43 -0500

    began sharing method

--- app/src/main/java/nyc/c4q/shannonalexander_navarro/memestudio/CreateMemeActivity.java
+++ app/src/main/java/nyc/c4q/shannonalexander_navarro/memestudio/CreateMemeActivity.java
@@ -1,21 +1,27 @@
 package nyc.c4q.shannonalexander_navarro.memestudio;
 
 import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
+
+import java.io.File;
 
 public class CreateMemeActivity extends AppCompatActivity {
 
     FloatingActionButton home_fab;
 
+    private FloatingActionButton shareMeme;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_meme);
 
         home_fab = (FloatingActionButton)findViewById(R.id.home);
+
         home_fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -25,7 +31,22 @@ public class CreateMemeActivity extends AppCompatActivity {
 
         });
 
+        shareMeme = (FloatingActionButton) findViewById(R.id.share);
 
+        shareMeme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sendMemeIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sendMemeIntent.setType("image/jpeg");
+
+                File savedPic = new File(
+                        Environment.getExternalStoragePublicDirectory(
+                                Environment.DIRECTORY_PICTURES),
+
+                        )
+                )
+            }
+        });
     }
 
 
